316b30a85d930ce80670f94ebac4abd293493645
vdbe <me@ewood.dev>
2026-03-13 23:31:00 +0100

    feat: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Personal QMK config environment";
+
+  inputs = {
+    nixpkgs.url = "https://nixpkgs.dev/channel/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      inherit (nixpkgs) lib;
+      inherit (lib.attrsets) genAttrs;
+
+      pkgsFor = pkgs: system: import pkgs { inherit system; };
+
+      allSystems = lib.systems.flakeExposed;
+
+      forAllSystems =
+        f:
+        genAttrs allSystems (
+          system:
+          f {
+            inherit system;
+            pkgs = pkgsFor nixpkgs system;
+          }
+        );
+    in
+    {
+      formatter = forAllSystems ({ pkgs, ... }: pkgs.nixfmt);
+
+      devShells = forAllSystems (
+        { pkgs, system, ... }:
+        let
+          default = qmk;
+          qmk = pkgs.mkShellNoCC {
+            packages = [
+              pkgs.qmk
+              pkgs.dos2unix
+            ];
+
+            shellHook = ''
+              export QMK_HOME="$(git rev-parse --show-toplevel)/qmk_firmware"
+            '';
+          };
+
+          nix = default.overrideAttrs (old: {
+            packages = (old.packages or []) ++ [
+              self.formatter.${system}
+              pkgs.nil
+              pkgs.statix
+            ];
+          });
+        in
+        {
+          inherit default qmk nix;
+        }
+      );
+    };
+}
